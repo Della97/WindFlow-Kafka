07332c2d1dfac8625a422d9f5aa93d88144fb154
Della97 <matteo.della.bartola@gmail.com>
2022-02-03 11:09:42 +0100

    test

--- tests/kafka_tests/test_kafka_1.cpp
+++ tests/kafka_tests/test_kafka_1.cpp
@@ -107,7 +107,7 @@ int main()
     size_t outputBactchSize = 1;
     int parallelism = 1;
     RdKafka::Message *msg;
-    std::vector<std::string> topics = "topic";
+    std::vector<std::string> topics = { "test" };
     Source_Shipper<tuple_t> *shipper;
     int32_t offset = 0;
     RdKafka::Conf *cconf;
