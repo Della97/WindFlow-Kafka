6081eac3314e9716195017e8cdff76d22ee7b3a5
Della97 <matteo.della.bartola@gmail.com>
2022-02-09 11:07:57 +0100

    fix

--- tests/kafka_tests/test_kafka_2.cpp
+++ tests/kafka_tests/test_kafka_2.cpp
@@ -82,18 +82,13 @@ class deser_functor
 public:
     bool operator()(RdKafka::Message &msg, Source_Shipper<tuple_t> &shipper)
     {
-        if (msg != NULL) {
-            tuple_t out;
-            std::cout << "Entered deser" << std::endl;
-            out.value = stoi(msg.key());
-            //out.value = msg.payload();
-            out.key = 0;
-            shipper.push(out);
-            return true;
-
-        } else {
-            return false;
-        }
+        tuple_t out;
+        std::cout << "Entered deser" << std::endl;
+        out.value = stoi(msg.key());
+        //out.value = msg.payload();
+        out.key = 0;
+        shipper.push(out);
+        return true;
     }
 };
 
