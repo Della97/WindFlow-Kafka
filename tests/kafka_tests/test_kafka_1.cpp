0ad01e88a741108c8d50f4b3107e790046031b2b
Della97 <matteo.della.bartola@gmail.com>
2022-02-10 23:35:37 +0100

    test partition

--- tests/kafka_tests/test_kafka_1.cpp
+++ tests/kafka_tests/test_kafka_1.cpp
@@ -1,182 +1,190 @@
-/**************************************************************************************
- *  Copyright (c) 2019- Gabriele Mencagli and Matteo Della Bartola
- *  
- *  This file is part of WindFlow.
- *  
- *  WindFlow is free software dual licensed under the GNU LGPL or MIT License.
- *  You can redistribute it and/or modify it under the terms of the
- *    * GNU Lesser General Public License as published by
- *      the Free Software Foundation, either version 3 of the License, or
- *      (at your option) any later version
- *    OR
- *    * MIT License: https://github.com/ParaGroup/WindFlow/blob/vers3.x/LICENSE.MIT
- *  
- *  WindFlow is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  You should have received a copy of the GNU Lesser General Public License and
- *  the MIT License along with WindFlow. If not, see <http://www.gnu.org/licenses/>
- *  and <http://opensource.org/licenses/MIT/>.
- **************************************************************************************
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <csignal>
+#include <cstring>
+
+#if _AIX
+#include <unistd.h>
+#endif
+
+/*
+ * Typical include path in a real application would be
+ * #include <librdkafka/rdkafkacpp.h>
  */
+#include <librdkafka/rdkafkacpp.h>
 
-/*  
- *  Test 1 of Kafka operators in WindFlow
- */ 
 
-#include<random>
-#include<string>
-#include<iostream>
-#include<functional>
-#include<windflow.hpp>
-#include<kafka/windflow_kafka.hpp>
+static volatile sig_atomic_t run = 1;
 
-using namespace std;
-using namespace wf;
-
-// tuple_t struct
-struct tuple_t
-{
-    int key;
-    int value;
-};
-
-// deserialization function (stub)
-bool deser_func(RdKafka::Message &msg, Source_Shipper<tuple_t> &shipper)
-{
-    return true;
+static void sigterm(int sig) {
+  run = 0;
 }
 
-// closing logic (stub)
-void closing_func(RuntimeContext &r) {}
 
-// deserialization functor (stub)
-class deser_functor
-{
-public:
-    bool operator()(RdKafka::Message &msg, Source_Shipper<tuple_t> &shipper)
-    {
-        return true;
+class ExampleDeliveryReportCb : public RdKafka::DeliveryReportCb {
+ public:
+  void dr_cb(RdKafka::Message &message) {
+    /* If message.err() is non-zero the message delivery failed permanently
+     * for the message. */
+    if (message.err())
+      std::cerr << "% Message delivery failed: " << message.errstr()
+                << std::endl;
+    else
+      std::cerr << "% Message delivered to topic " << message.topic_name()
+                << " [" << message.partition() << "] at offset "
+                << message.offset() << std::endl;
+  }
+};
+
+int main(int argc, char **argv) {
+
+  std::string brokers = "localhost";
+  std::string topic   = "test";
+
+  /*
+   * Create configuration object
+   */
+  RdKafka::Conf *conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
+
+  std::string errstr;
+
+  /* Set bootstrap broker(s) as a comma-separated list of
+   * host or host:port (default port 9092).
+   * librdkafka will use the bootstrap brokers to acquire the full
+   * set of brokers from the cluster. */
+  if (conf->set("bootstrap.servers", brokers, errstr) !=
+      RdKafka::Conf::CONF_OK) {
+    std::cerr << errstr << std::endl;
+    exit(1);
+  }
+
+  signal(SIGINT, sigterm);
+  signal(SIGTERM, sigterm);
+
+  /* Set the delivery report callback.
+   * This callback will be called once per message to inform
+   * the application if delivery succeeded or failed.
+   * See dr_msg_cb() above.
+   * The callback is only triggered from ::poll() and ::flush().
+   *
+   * IMPORTANT:
+   * Make sure the DeliveryReport instance outlives the Producer object,
+   * either by putting it on the heap or as in this case as a stack variable
+   * that will NOT go out of scope for the duration of the Producer object.
+   */
+  ExampleDeliveryReportCb ex_dr_cb;
+
+  if (conf->set("dr_cb", &ex_dr_cb, errstr) != RdKafka::Conf::CONF_OK) {
+    std::cerr << errstr << std::endl;
+    exit(1);
+  }
+
+  /*
+   * Create producer instance.
+   */
+  RdKafka::Producer *producer = RdKafka::Producer::create(conf, errstr);
+  if (!producer) {
+    std::cerr << "Failed to create producer: " << errstr << std::endl;
+    exit(1);
+  }
+
+  delete conf;
+
+  /*
+   * Read messages from stdin and produce to broker.
+   */
+  std::cout << "% Type message value and hit enter "
+            << "to produce message." << std::endl;
+
+  for (std::string line; run && std::getline(std::cin, line);) {
+    if (line.empty()) {
+      producer->poll(0);
+      continue;
     }
-};
 
-// closing functor (stub)
-class closing_functor
-{
-public:
-    void operator()(RuntimeContext &r) {}
-};
+    /*
+     * Send/Produce message.
+     * This is an asynchronous call, on success it will only
+     * enqueue the message on the internal producer queue.
+     * The actual delivery attempts to the broker are handled
+     * by background threads.
+     * The previously registered delivery report callback
+     * is used to signal back to the application when the message
+     * has been delivered (or failed permanently after retries).
+     */
+  retry:
+    RdKafka::ErrorCode err = producer->produce(
+        /* Topic name */
+        topic,
+        /* Any Partition: the builtin partitioner will be
+         * used to assign the message to a topic based
+         * on the message key, or random partition if
+         * the key is not set. */
+        1,
+        /* Make a copy of the value */
+        RdKafka::Producer::RK_MSG_COPY /* Copy payload */,
+        /* Value */
+        const_cast<char *>(line.c_str()), line.size(),
+        /* Key */
+        NULL, 0,
+        /* Timestamp (defaults to current time) */
+        0,
+        /* Message headers, if any */
+        NULL,
+        /* Per-message opaque value passed to
+         * delivery report */
+        NULL);
 
-// sink functor
+    if (err != RdKafka::ERR_NO_ERROR) {
+      std::cerr << "% Failed to produce to topic " << topic << ": "
+                << RdKafka::err2str(err) << std::endl;
 
-// Sink functor
-class Sink_Functor
-{
-private:
-    size_t received; // counter of received results
-    long totalsum;
+      if (err == RdKafka::ERR__QUEUE_FULL) {
+        /* If the internal queue is full, wait for
+         * messages to be delivered and then retry.
+         * The internal queue represents both
+         * messages to be sent and messages that have
+         * been sent or failed, awaiting their
+         * delivery report callback to be called.
+         *
+         * The internal queue is limited by the
+         * configuration property
+         * queue.buffering.max.messages */
+        producer->poll(1000 /*block for max 1000ms*/);
+        goto retry;
+      }
 
-public:
-    // Constructor
-    Sink_Functor():
-                 received(0),
-                 totalsum(0) {}
-
-    // operator()
-    void operator()(optional<tuple_t> &out)
-    {
-        if (out) {
-            received++;
-            totalsum += (*out).value;
-        }
-        else {
-            // printf("Received: %ld results, total sum: %ld\n", received, totalsum);
-            //global_sum.fetch_add(totalsum);
-        }
+    } else {
+      std::cerr << "% Enqueued message (" << line.size() << " bytes) "
+                << "for topic " << topic << std::endl;
     }
-};
 
-// main
-int main()
-{
-    std::cout << "Test creazione Kafka_Source mediante costruttore raw" << std::endl;
-    string name = "my_kafka_source";
-    string brokers = "localhost";
-    string groupid = "gid";
-    size_t outputBactchSize = 1;
-    int parallelism = 1;
-    RdKafka::Message *msg;
-    std::vector<std::string> topics = { "test" };
-    Source_Shipper<tuple_t> *shipper;
-    int32_t offset = 0;
-    RdKafka::Conf *cconf;
-    RdKafka::Conf *tconf;
+    /* A producer application should continually serve
+     * the delivery report queue by calling poll()
+     * at frequent intervals.
+     * Either put the poll call in your main loop, or in a
+     * dedicated thread, or call it after every produce() call.
+     * Just make sure that poll() is still called
+     * during periods where you are not producing any messages
+     * to make sure previously produced messages have their
+     * delivery report callback served (and any other callbacks
+     * you register). */
+    producer->poll(0);
+  }
 
-    size_t sink1_degree = 1;
+  /* Wait for final messages to be delivered or fail.
+   * flush() is an abstraction over poll() which
+   * waits for all messages to be delivered. */
+  std::cerr << "% Flushing final messages..." << std::endl;
+  producer->flush(10 * 1000 /* wait for max 10 seconds */);
 
-    Kafka_Source source1 = Kafka_Source(deser_func, name, outputBactchSize, brokers, topics, groupid, parallelism, offset, closing_func);
-    std::cout << "Creazione con funzioni -> OK!" << std::endl;
+  if (producer->outq_len() > 0)
+    std::cerr << "% " << producer->outq_len()
+              << " message(s) were not delivered" << std::endl;
 
-    auto deser_lambda = [](RdKafka::Message &msg, Source_Shipper<tuple_t> &shipper) { return true; };
-    auto closing_lambda = [](RuntimeContext &) { return; };
+  delete producer;
 
-    Kafka_Source source2 = Kafka_Source(deser_lambda, name, outputBactchSize, brokers, topics, groupid, parallelism, offset, closing_lambda);
-    std::cout << "Creazione con lambda -> OK!" << std::endl;    
-
-    deser_functor deser_functor;
-    closing_functor closing_functor;
-    Kafka_Source source3 = Kafka_Source(deser_functor, name, outputBactchSize, brokers, topics, groupid, parallelism, offset, closing_functor);
-    std::cout << "Creazione con funtori -> OK!" << std::endl;
-
-    std::cout << "Test creazione Kafka_Source mediante builder" << std::endl;
-    Kafka_Source source4 = Kafka_Source_Builder(deser_func)
-                                .withName(name)
-                                .withOutputBatchSize(outputBactchSize)
-                                .withClosingFunction(closing_func)
-                                .withBrokers(brokers)
-                                .withTopics(topics)
-                                .withGroupID(groupid)
-                                .withPartition(parallelism)
-                                .withOffset(offset)
-                                .build();
-    std::cout << "Creazione con builder tramite funzioni -> OK!" <<  std::endl;
-
-    Kafka_Source source5 = Kafka_Source_Builder(deser_lambda)
-                                .withName(name)
-                                .withOutputBatchSize(outputBactchSize)
-                                .withClosingFunction(closing_lambda)
-                                .withBrokers(brokers)
-                                .withTopics(topics)
-                                .withGroupID(groupid)
-                                .withPartition(parallelism)
-                                .withOffset(offset)
-                                .build();
-    std::cout << "Creazione con builder tramite lambda -> OK!" <<  std::endl;
-
-    Kafka_Source source6 = Kafka_Source_Builder(deser_functor)
-                                .withName(name)
-                                .withOutputBatchSize(outputBactchSize)
-                                .withClosingFunction(closing_functor)
-                                .withBrokers(brokers)
-                                .withTopics(topics)
-                                .withGroupID(groupid)
-                                .withPartition(parallelism)
-                                .withOffset(offset)
-                                .build();
-    std::cout << "Creazione con builder tramite funtori -> OK!" <<  std::endl;
-
-    PipeGraph graph("test_tracing_1", Execution_Mode_t::DEFAULT, Time_Policy_t::EVENT_TIME);
-    MultiPipe &pipe = graph.add_source(source6);
-
-    //SINK
-    Sink_Functor sink_functor;
-        Sink sink1 = Sink_Builder(sink_functor)
-                        .withName("sink1")
-                        .withParallelism(sink1_degree)
-                        .build();
-        pipe.chain_sink(sink1);
-    graph.run();
-    std::cout << "Exiting..." <<  std::endl;
-    return 0;
+  return 0;
 }
