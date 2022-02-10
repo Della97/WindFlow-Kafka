0594466acb994e59f59674ecec6fad7f6d6b414c
Della97 <matteo.della.bartola@gmail.com>
2022-02-10 11:34:42 +0100

    Update test_kafka_2.cpp

--- tests/kafka_tests/test_kafka_2.cpp
+++ tests/kafka_tests/test_kafka_2.cpp
@@ -62,7 +62,7 @@ public:
             std::cout << "Received: " << std::endl;
         }
         else {
-            std::cout << "Received nothing: " << out.value << std::endl;
+            std::cout << "Received nothing: " << std::endl;
         }
     }
 };
