8aba0ba2f87226d71d821d7291bd4474e98f83cb
Della97 <matteo.della.bartola@gmail.com>
2022-02-16 01:04:28 +0100

    Update test_kafka_2.cpp

--- tests/kafka_tests/test_kafka_2.cpp
+++ tests/kafka_tests/test_kafka_2.cpp
@@ -131,18 +131,18 @@ int main()
     std::string topic2 = "prova";
     std::string topic3 = "topic";
 
-    Kafka_Source source1 = Kafka_Source(deser_func, name, outputBactchSize, brokers, topic1, groupid, parallelism, offset, closing_func);
+    Kafka_Source source1 = Kafka_Source(deser_func, name, outputBactchSize, brokers, topics, groupid, parallelism, offset, closing_func);
     std::cout << "Creazione con funzioni -> OK!" << std::endl;
 
     auto deser_lambda = [](RdKafka::Message &msg, Source_Shipper<tuple_t> &shipper) { return true; };
     auto closing_lambda = [](RuntimeContext &) { return; };
 
-    Kafka_Source source2 = Kafka_Source(deser_lambda, name, outputBactchSize, brokers, topic1, groupid, parallelism, offset, closing_lambda);
+    Kafka_Source source2 = Kafka_Source(deser_lambda, name, outputBactchSize, brokers, topics, groupid, parallelism, offset, closing_lambda);
     std::cout << "Creazione con lambda -> OK!" << std::endl;    
 
     deser_functor deser_functor;
     closing_functor closing_functor;
-    Kafka_Source source3 = Kafka_Source(deser_functor, name, outputBactchSize, brokers, topic1, groupid, parallelism, offset, closing_functor);
+    Kafka_Source source3 = Kafka_Source(deser_functor, name, outputBactchSize, brokers, topics, groupid, parallelism, offset, closing_functor);
     std::cout << "Creazione con funtori -> OK!" << std::endl;
 
     std::cout << "Test creazione Kafka_Source mediante builder" << std::endl;
