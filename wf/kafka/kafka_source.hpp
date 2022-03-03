3dcdf38f3833670831aa5c3a5ebfded523122a3a
Della97 <matteo.della.bartola@gmail.com>
2022-03-03 19:36:55 +0100

    Update kafka_source.hpp

--- wf/kafka/kafka_source.hpp
+++ wf/kafka/kafka_source.hpp
@@ -314,7 +314,6 @@ public:
         
         /* Subscribe to topics */
 
-        std::cout << "% Created consumer " << consumer->name() << std::endl;
         RdKafka::ErrorCode err = consumer->subscribe(topics);
         if (err) {
             std::cerr << "Failed to subscribe to " << topics.size()
