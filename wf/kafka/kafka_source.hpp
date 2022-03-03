233cba815dfce0df83ae806e14f7a811f5f9bb91
Della97 <matteo.della.bartola@gmail.com>
2022-03-03 19:00:41 +0100

    Update kafka_source.hpp

--- wf/kafka/kafka_source.hpp
+++ wf/kafka/kafka_source.hpp
@@ -463,7 +463,6 @@ private:
     int32_t partition;
     int32_t offset;
     pthread_barrier_t bar;
-    pthread_barrierattr_t barattr;
 
     // Configure the Kafka_Source to receive batches instead of individual inputs (cannot be called for the Kafka_Source)
     void receiveBatches(bool _input_batching) override
@@ -599,8 +598,7 @@ public:
             exit(EXIT_FAILURE);
         }
         std::cout << "parallelism : " << parallelism << std::endl;
-        pthread_barrierattr_setpshared(&barattr, PTHREAD_PROCESS_SHARED);
-        pthread_barrier_init(&bar, &barattr, parallelism);
+        pthread_barrier_init(&bar, NULL, parallelism);
 
         //parallelims check but we dont know the number of partitions
         //pthread barrier
