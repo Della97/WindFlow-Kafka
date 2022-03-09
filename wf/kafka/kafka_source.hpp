1bd590dee4f08316999e406e5fa98b5430a0a36c
Della97 <matteo.della.bartola@gmail.com>
2022-03-09 11:55:55 +0100

    Update kafka_source.hpp

--- wf/kafka/kafka_source.hpp
+++ wf/kafka/kafka_source.hpp
@@ -334,8 +334,15 @@ public:
         //std::cout << "before barrier id: " << consumer->name() << std::endl;
 
         pthread_barrier_wait(bar);
-        consumer->poll(0);
-        consumer->assignment(partitions);
+        while (fetch) {
+            consumer->poll(0);
+            consumer->assignment(partitions);
+            if (!(partitions.empty())) {
+                fetch = false;
+            }
+        }
+        //consumer->poll(0);
+        //consumer->assignment(partitions);
 #if defined (WF_TRACING_ENABLED)
         stats_record = Stats_Record(opName, std::to_string(context.getReplicaIndex()), false, false);
 #endif
