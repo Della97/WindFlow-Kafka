2145047ff4e4949a23804bd6ebf96f07ea71e72d
Della97 <matteo.della.bartola@gmail.com>
2022-03-10 10:54:34 +0100

    Update kafka_source.hpp

--- wf/kafka/kafka_source.hpp
+++ wf/kafka/kafka_source.hpp
@@ -76,8 +76,8 @@ class ExampleRebalanceCb : public RdKafka::RebalanceCb {
     RdKafka::Error *error      = NULL;
     RdKafka::ErrorCode ret_err = RdKafka::ERR_NO_ERROR;
     //std::cout << "OOOOOOOOOOOOOOOOOOOOOOO" << offset << std::endl;
-    for (int i = 0, i < 6, i++) {
-        std::cout << "AHHHHHHHHHHHHHHHHH " << topics.size() << offsets.size() << std::endl;
+    for (int i = 0; i < 6; i++) {
+        std::cout << "AHHHHHHHHHHHHHHHHH " << topics.size() << "   " << offsets.size() << std::endl;
 
     }
 
