83245047cfee470a9c4eb3baa9f98b3324f2205f
Della97 <matteo.della.bartola@gmail.com>
2022-03-15 16:13:52 +0100

    Update kafka_source.hpp

--- wf/kafka/kafka_source.hpp
+++ wf/kafka/kafka_source.hpp
@@ -659,6 +659,7 @@ public:
             std::cerr << RED << "WindFlow Error: Kafka_Source has parallelism zero" << DEFAULT_COLOR << std::endl;
             exit(EXIT_FAILURE);
         }
+        std::cout << "PROIMA DELLE REPLICHE" << parallelism << std::endl;
         std::cout << "topics -> " << topics.size() << " offsets -> " << offset.size() << std::endl;
         if (offset.size() < topics.size()) {
             std::cerr << RED << "WindFlow Error: Number of offsets given are less than the number of topics" << DEFAULT_COLOR << std::endl;
