3a006a09a4d8467c322e000638bf93f71c63294f
Della97 <matteo.della.bartola@gmail.com>
2022-06-17 17:29:26 +0200

    added delay

--- tests/kafka_tests/tests/FraudDetection/src/generator.cpp
+++ tests/kafka_tests/tests/FraudDetection/src/generator.cpp
@@ -190,6 +190,7 @@ int main(int argc, char* argv[]) {
                                                 NULL);    //
 
             producer->poll(0);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
         next_tuple_idx = (next_tuple_idx + 1) % dataset.size();   // index of the next tuple to be sent (if any)
         count++;
         index++;
