86c5228ef735e4d62e75b6572f3ce97f83e706a0
Della97 <matteo.della.bartola@gmail.com>
2022-06-17 15:58:31 +0200

    sink

--- tests/kafka_tests/tests/SD/includes/nodes/sink.hpp
+++ tests/kafka_tests/tests/SD/includes/nodes/sink.hpp
@@ -88,7 +88,7 @@ public:
         RdKafka::Producer *producer = rc.getProducer();
         msg = out.incremental_average;
 
-        std::cout << "MEDIA SINK: " << tmpp / arrived << std::endl;
+        std::cout << "MEDIA SINK: " << tmpp / processed << std::endl;
         std::cout << "MESSAGGIO: " << msg << std::endl;
 
         tmp.partition = rc.getReplicaIndex();
