b6d9d0702bdbd4bafb632c21e62ca683e1b87302
Della97 <matteo.della.bartola@gmail.com>
2022-06-22 23:34:12 +0200

    fix

--- tests/kafka_tests/tests/FD/includes/nodes/sink_kafka.hpp
+++ tests/kafka_tests/tests/FD/includes/nodes/sink_kafka.hpp
@@ -85,7 +85,7 @@ public:
 
             // always evaluate latency when compiling with FF_BOUNDED_BUFFER MACRO set
             unsigned long tuple_latency = (current_time_nsecs() - (out).ts) / 1e03;
-            std::cout << "SINK: " <<  tuple_latency << std::endl;
+            //std::cout << "SINK: " <<  tuple_latency << std::endl;
             tmpp = tmpp + tuple_latency;
             processed++;        // tuples counter
 
@@ -100,7 +100,7 @@ public:
             wf::wf_kafka_sink_msg tmp;
             RdKafka::Producer *producer = rc.getProducer();
             std::string msg = "Ricevuto fraud entity_id: " + ((out).entity_id);
-            //std::cout << "MEDIA SINK: " << tmpp / processed << std::endl;
+            std::cout << "MEDIA SINK: " << tmpp / processed << std::endl;
 
             if (processed == 400000) {
                 //util::metric_group.add("latency", latency_sampler);
